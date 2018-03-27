f2aab1f33d5a19e92a0b392923c0958cd0210b96
Carter Gruss <carter.gruss@anthem.com>
2018-03-27 18:18:23 -0500

    Created initial Header component, as well as a miscellaneous Card component.
    I'm preparing the app to be separate into sections, and to utilize a separate Jekyll app.

--- angular/app/app.module.ts
+++ angular/app/app.module.ts
@@ -1,16 +1,20 @@
+import { HeaderModule } from './header/header.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { LibraryModule } from '../../node_modules/cc-component-library/src/lib/library.module';
 
 import { AppComponent } from './app.component';
 
 
 @NgModule({
+  imports: [
+    BrowserModule,
+    LibraryModule,
+    HeaderModule,
+  ],
   declarations: [
     AppComponent
-  ],
-  imports: [
-    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
