67f2048962bda36e52f51747d9724811775ddfd3
Crayton Smith <crayton.smith@anthem.com>
2018-04-20 12:20:08 -0500

    updating with compodoc and pushing this package.json.lock file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { LibraryModule } from '../../node_modules/cc-component-library/src/lib/library.module';
 import { AppComponent } from './app.component';
+import { InternalButtonComponent } from './internal-button/internal-button.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InternalButtonComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/internal-button/internal-button.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-internal-button',
+  templateUrl: './internal-button.component.html',
+  styleUrls: ['./internal-button.component.scss']
+})
+export class InternalButtonComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
